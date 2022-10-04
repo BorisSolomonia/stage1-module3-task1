911bfae61b70249596226e503863ac8cb51e6467
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-10-04 12:44:46 +0400

    Update Generics.java

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -2,26 +2,27 @@ package com.epam.mjc.generics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Generics {
 
     //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
-        firstList.add(name);
-        List secondList = new ArrayList<>();
-        secondList.add(firstList);
+    public <T> List<T> boxingMethod(String name) {
+        List<T> firstList = new ArrayList<>();
+        firstList.add((T) name);
+        List<T> secondList = new ArrayList<>();
+        secondList.add((T) firstList);
         return secondList;
     }
 
     //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    public <T> Object genericMethod(T data) {
         return data;
     }
 
     //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
-        consumer.addAll(producer);
+    public <G, U> void cloneMethod(List<G> consumer, List<U> producer) {
+        consumer.addAll(producer.stream().map(x -> (G) x).collect(Collectors.toList()));
     }
 
 }
